cc2578a00d6398f64ab6f9d8038bf66da1543287
jluis <J. Luis>
2017-03-09 02:37:11 -0600

    Analisis de encabezados, errores comunes y manejo de diccionario.

--- moodlecrawl.pl
+++ moodlecrawl.pl
@@ -24,16 +24,127 @@ my $uaSSL = LWP::UserAgent->new(
     SSL_ca_path => "/etc/ssl/certs/",
 );
 $ua->agent('Mozilla/5.0');
-my $reqSSL = HTTP::Request->new(GET => 'https://aula.cert.unam.mx/');
-#my $req = HTTP::Request->new(GET => 'http://192.168.13.149/');
+#my $url = 'http://192.168.13.149/';
+my $url = 'https://aula.cert.unam.mx/';
+#my $req = HTTP::Request->new(GET => $url);
+my $reqSSL = HTTP::Request->new(GET => $url);
+#my $req = HTTP::Request->new(GET => 'http://bpya.fciencias.unam.mx/moodle/');
 my $res = $uaSSL->request($reqSSL);
 #my $res = $ua->request($req);
 
+print "Codigo de respuesta: ".$res->code."\n\n";
+
 if ($res->is_success) { 
-    analizaEncabezado(\$res);
+    analizaEncabezado($res);
 }
 else {
     print "Failed: ", $res->status_line, "\n";
+}
+analizaCodigosError($url);
+
+my $dic = "dicMoodle2";
+
+revisaDiccionario($url, $dic);
+
+sub analizaCodigosError{
+    my $url = shift @_;
+    my ($ua, $res403, $res404, $res500, $req);
+
+    print "Analisis de error 404:\n";
+    $ua = LWP::UserAgent->new();
+    $ua->agent('Mozilla/5.0');
+    $req = HTTP::Request->new(GET => $url.'archivoParaError404.php');
+    $res404 = $ua->request($req);
+    print "\tURL para generar error 404: ".$url."archivoParaError404.php\n";
+    print "\tCodigo de respuesta: ".$res404->code."\n";
+    if (grep(/Apache\/.*[0-9]+\.[0-9]+/, $res404->content))
+    {
+	print "\tDiagnostico: el servidor revela informacion que deberia mantenerse como privada.\n";
+	print "\tRecomendacion: incluir 'ServerTokens Prod' y 'ServerSignature' en la configuracion de Apache para reducir la informacion divulgada.\n";
+	print "\tTambien se dede definir la respuesta para el error 404 en el servidor Web\n\n";
+    }
+    else
+    {
+	print "\tDiagnostico: el servidor no revela informacion privada.\n\n";
+    }
+
+    print "Analisis de error 403:\n";
+    $ua = LWP::UserAgent->new();
+    $ua->agent('Mozilla/5.0');
+    $req = HTTP::Request->new(GET => $url.'archivoParaError403.php');
+    $res403 = $ua->request($req);
+    print "\tURL para generar error 404: ".$url."archivoParaError403.php\n";
+    print "\tCodigo de respuesta: ".$res403->code."\n";
+    if (grep(/Apache\/.*[0-9]+\.[0-9]+/, $res403->content))
+    {
+	print "\tDiagnostico: el servidor revela informacion que deberia mantenerse como privada.\n";
+	print "\tRecomendacion: incluir 'ServerTokens Prod' y 'ServerSignature' en la configuracion de Apache para reducir la informacion divulgada.\n";
+	print "\tTambien se dede definir la respuesta para el error 404 en el servidor Web\n\n";
+    }
+    else
+    {
+	print "\tDiagnostico: el servidor no revela informacion privada.\n\n";
+    }
+
+    print "Analisis de error 500:\n";
+    $req = HTTP::Request->new(GET => $url.'version.php');
+    $res500 = $ua->request($req);
+    print "\tURL y metodo para generar error 500: GET ".$url."version.php\n";
+    print "\tCodigo de respuesta: ".$res500->code."\n";
+    if (grep(/Apache\/.*[0-9]+\.[0-9]+/, $res500->content))
+    {
+	print "\tDiagnostico: el servidor revela informacion que deberia mantenerse como privada.\n";
+	print "\tRecomendacion: incluir 'ServerTokens Prod' y 'ServerSignature' en la configuracion de Apache para reducir la informacion divulgada.\n";
+	print "\tTambien se dede definir la respuesta para el error 500 en el servidor Web\n\n";
+    }
+    else
+    {
+	print "\tDiagnostico: el servidor no revela informacion privada.\n\n";
+    }
+}
+
+
+sub revisaDiccionario{
+    my ($url, $dicFile) = @_;
+    my ($ua, $res, $req, $urlDir);
+
+    print "Analisis de diccionario:\n";
+    $ua = LWP::UserAgent->new();
+    $ua->agent('Mozilla/5.0');
+
+    open (DIRECTORIOS, $dicFile)
+	or die "No se puede abrir el archivo de contrasenias\n";
+
+    while (<DIRECTORIOS>) {
+	chomp($_);
+	$urlDir = $url.$_;
+	$req = HTTP::Request->new(GET => $urlDir);
+	$res = $ua->request($req);
+	print "$urlDir\n";
+	print "\tDiagnostico: ";
+	if ($res->code == 200){
+	    print "El directorio existe y es accesible.\n\n";
+	}
+	else{
+	    if ($res->code == 403){
+		print "El directorio existe pero se requieren privilegios para acceder.\n\n";
+	    }
+	    else{
+		if ($res->code == 404){
+		    print "El directorio no existe.\n\n";
+		}
+		else{
+		    if ($res->code >= 300 && $res->code < 400){
+			print "La peticion fue redirigida.\n\n";
+		    }
+		    else{
+			print "El servidor reporto el error ".$res->code."\n\n";
+		    }
+		}
+	    }
+	}
+    }
+    close (DIRECTORIOS);
 }
 
 
@@ -140,10 +251,4 @@ sub muestraAyuda {
 
 
 
-#http://search.cpan.org/~ether/HTTP-Message-6.11/lib/HTTP/Response.pm
-#http://search.cpan.org/~oalders/libwww-perl-6.23/lib/LWP/UserAgent.pm
-#http://stackoverflow.com/questions/4022463/how-can-i-extract-non-standard-http-headers-using-perls-lwp
-#http://search.cpan.org/~ether/HTTP-Message-6.11/lib/HTTP/Headers.pm
-#http://search.cpan.org/~ether/HTTP-Message-6.11/lib/HTTP/Message.pm
-###
-#http://lwp.interglacial.com/ch03_05.htm
+
