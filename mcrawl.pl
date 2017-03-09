f89018b932905d31b174af326ec44c986bad5ed8
Mario Arturo Perez Rangel <arturo@debpm2.fciencias.unam.mx>
2017-03-09 00:53:04 -0600

    Sin funcion help

--- mcrawl.pl
+++ mcrawl.pl
@@ -34,10 +34,24 @@ if ($opciones{help}){
 procesaOpciones(\%opciones, \%params);
 adjustParams(\%params, \%sslopts);
 
+
+my $ua = LWP::UserAgent->new ( ssl_opts => \%sslopts );
+my $req = HTTP::Request->new ('GET' => $params{url});
+my $res = $ua->request($req);
+
+my $tree = HTML::TreeBuilder->new_from_content($res->content);
+my @ah = $tree->look_down ('_tag', 'a', 'href', qr/.+/);
+my @im = $tree->look_down ('_tag', 'img', 'src', qr/.+/);
+#for my $k (@ah) {
+#    print $k->attr('href'), "\n";
+#}
+for my $k (@im) {
+    print $k->attr('src'), "\n";
+}
 # Hacemos peticiones con el metodo HEAD hasta determinar si en la url proporcionada
 # el servidor despacha moodle, o se redirecciona a un sitio con SSL. Tambien se detecta
 # si el certificado del servidor es emitido por alguna CA valida. Si es autofirmado o
-# presenta problemas saltamos la validacion del certificado
+# presenta problemas saltamos la validacion del certificado.
 sub adjustParams {
     my ($p, $ssl) = @_;
     my ($ua, $req, $res);
@@ -46,22 +60,20 @@ sub adjustParams {
 	print "Demasiados intentos de ajustar los parametros de conexion.\n";
 	exit (-1);
     }
-    $ua = LWP::UserAgent->new(
-	                      ssl_opts => $ssl,
-	                     );
+    $ua = LWP::UserAgent->new( ssl_opts => $ssl );
 
     $ua->agent('Mozilla/5.0');
 
     $req = HTTP::Request->new(HEAD => $params{url});
     $res = $ua->request($req);
 
-    print $res->code, "\n";
+    # Vemos la respuesta del servidor
     if ($res->code == 200) { # Todo bien
 	$p->{loop} = 0;
 	return;
-    } elsif ($res->code == 301 or $res->code == 302)  { # Redireccionar
+    } elsif ($res->code == 301 or $res->code == 302)  { # Redirecciona a otro recurso
 	my $location = $res->headers->{'location'};
-	if ($location =~ m|^(https?)(://)([\w\d._-]+)(:\d+)?(/.*)$|) {
+	if ($location =~ m|^(https?)(://)([\w\d._-]+)(:\d+)?(/.*)$|) {  # Es una URL?
 	    $p->{scheme} = $1;
 	    $p->{host} = $3;
 	    if ($4) {
@@ -69,17 +81,23 @@ sub adjustParams {
 		$p->{port} =~ s/^://;
 	    }
 	    $p->{uri} = $5;
-	} elsif ($location =~ m|^/.*|) {
-	    $p->{uri} = $op->{url};
+	} elsif ($location =~ m|^/.*|) {   # Es la ruta de un nuevo recurso ?
+	    $p->{uri} = $location;
+	    $p->{url} = join '', $p->{scheme}, '://', $p->{host},
+	                         ($p->{port}) ? ':'.$p->{port} : '',
+                                 $p->{uri};
+	} else { # Es el nombre de otro recurso
+	    $p->{url} =~ s|/([^/]+)$|/$location|;
+	    $p->{uri} =~ s|/([^/]+)$|/$location|;
 	}
-    } elsif ($res->code == 403) {
+    } elsif ($res->code == 403) {  # No tenemos acceso al recurso
 	print "No se tiene acceso a este recurso.\n";
 	exit (-1);
     } elsif ($res->code == 500) {
-	if ($res->status_line =~ /certificate verify failed/) {
+	if ($res->status_line =~ /certificate verify failed/) {  # No se pudo verificar el emisor del certificado
 	    $ssl->{SSL_verify_mode} = SSL_VERIFY_NONE;
 	    $ssl->{verify_hostname} = 0;
-	    adjustParams($p, $ssl);
+	    adjustParams($p, $ssl);            # Intentamos acceder al recurso sin verificar el certificado
 	}
     }
 }
@@ -93,7 +111,12 @@ sub adjustParams {
 ##
 sub procesaOpciones {
     my ($op, $p) = @_;
-    
+
+    if ($op->{ip} and $op->{url}) { # Solo se puede usar una, tomaremos url por default.
+	print "Ignorando la direccion ip.\n";
+	$p->{ip} = 0;
+    }
+
     if ($op->{dict}) {
 	my $nl = (stat $op->{dict})[3];
 	$nl = 0 if (!$nl);
@@ -103,6 +126,8 @@ sub procesaOpciones {
 	}
 	$p->{dict} = $op->{dict};
     }
+
+    # En vez de nombre de dominio se paso una ip
     if ($op->{ip}){
 	 # Es una direccion ip valida?
 	if ($op->{ip} =~ /^\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b$/) {
@@ -115,6 +140,8 @@ sub procesaOpciones {
 	    exit (3);
 	}
     }
+
+    # La url del moodle
     if ($op->{url}){
 	# Desmenuza la url en schema, host, puerto y uri
 	if ($op->{url} =~ m|^(https?)(://)([\w\d._-]+)(:\d+)?(/.*)$|) {
@@ -135,9 +162,13 @@ sub procesaOpciones {
 	    exit (4);
 	}
     }
+
+    # Bandera para indicar la generacion de un reporte
     if($op->{report}) {
 	$p->{report} = 1;
     }
+
+    # Manejo de login y password.
     if ($op->{login} and $op->{password}) {
 	if ($op->{login} =~ /^(?:[\w\d._-]+@)?[\w\d._-]+$/) {
 	    $p->{login} = $op->{login};
@@ -172,20 +203,7 @@ sub procesaOpciones {
 	print "Necesita un login para ese password\n";
 	exit (6);
     }
-#    if ($op->{ufile}){
-#	if ((stat($op->{ufile}))[3] > 0) { # El archivo existe si el numero de ligas en el sistema de archivo es mayor a cero
-#	    $p->{ufile} = $op->{ufile};
-#	} else {                           # No existe asi que usamos uno por defecto
-#	    $p->{ufile} = 'usernames.txt';
-#	}
-#    }
-#    if ($op->{pfile}){
-#	if ((stat($op->{pfile}))[3] > 0) { # El archivo existe si el numero de ligas en el sistema de archivo es mayor a cero
-#	    $p->{pfile} = $op->{pfile};
-#	} else {                           # No existe asi que usamos uno por defecto.
-#	    $p->{pfile} = 'passwords.txt';
-#	}
-#    }
+
     return $p;
 }
 
@@ -193,27 +211,5 @@ sub procesaOpciones {
 ## muestraAyuda muestra al usuario como se debe usar este programa.
 ##
 sub muestraAyuda {
-    print "$0 implementa dos tipos de ataque: fuerza bruta y de diccionario, contra un objetivo determinado.\n\n";
-    print "La forma de usarlo es:\n";
-    print "  $0 [--help|-h] [--http|--https] [--basic|-b|--digest|-d] \n";
-    print ' 'x (length($0)+3), "--host <nombre de host>|--ip <ip>  [[--puerto|-p] <puerto>] \n";
-    print ' 'x (length($0)+3), "--uri <uri> [--usuario <usuario>] [--ufile <archivo>] [--pfile <archivo>]  \n\n";
-    print "Donde:\n";
-    print "--help o -h\t Muestra esta ayuda\n";
-    print "-http\t\t Implica hacer conexiones con el protocolo HTTP.\n";
-    print "-https\t\t Implican hacer conexiones con el protocolo HTTP median SSL/TLS.\n";
-    print "--basic o -b\t Dirige el ataque usando el metodo de autenticacion BASIC\n";
-    print "--digest o -d\t Dirige el ataque usando el metodo de autenticacion DIGEST\n";
-    print "--host\t\t Nombre de dominio del objetivo\n";
-    print "--ip\t\t Direccion ip del objetivo\n";
-    print "--puerto o -p\t El puerto TCP del objetivo\n";
-    print "--uri o -u\t La ruta dentro del recurso dentro del objetivo, ej.: /webdav/\n";
-    print "--usuario\t El nombre de usuario a usar en el ataque.\n";
-    print "--ufile\t\t Nombre de un archivo con nombres de usuario, uno por linea.\n";
-    print "--pfile\t\t Nombre de un archivo con contrasenias de usuario, uno por linea.\n\n";
-    print "Los parametros --host y --uri no se pueden predeterminar por lo que es obligatorio su aparicion en la linea de comandos.\n\n";
-    print "De manera predeterminada se usa el protocolo http en el puerto 80.\n\n";
-    print "Los parametros --host e --ip son excluyentes, no pueden ir ambos en la misma linea de comandos.\n\n";
-    print "Todos los demas parametros son opcionales.\n\n";
 }
 
