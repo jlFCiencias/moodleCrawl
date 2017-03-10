5401ceb39f7423caa5f3eb16abadd61dbb76810f
Mario Arturo Perez Rangel <arturo@debpm2.fciencias.unam.mx>
2017-03-10 00:21:51 -0600

    Recopilacion de directorios y URLs

--- mcrawl.pl
+++ mcrawl.pl
@@ -15,39 +15,133 @@ use LWP::UserAgent;
 use HTML::TreeBuilder;
 
 my %params = (loop => 0);
-my %opciones;
-my %sslopts=();
+my (%options, %dir_found, %sslopts);
+my (@url_to_visit, @visited_url);
+
 #my %sslopts=(SSL_verify_mode => SSL_VERIFY_NONE,
 #	     verify_hostname => 0,
 #	     SSL_ca_path => IO::Socket::SSL::default_ca(),);
 
-GetOptions (\%opciones, 'help|h', 'ip|s=s', 'dict|d=s', 'report|r',
-	                'login|l=s', 'password|p=s', 'url=s',
+GetOptions (\%options, 'help|h', 'ip|s=s', 'dict|d=s', 'report|r',
+	                'login|l=s', 'password|p=s', 'url=s', 'debug',
 	    );
 
-if ($opciones{help}){
+if ($options{help}){
     muestraAyuda();
     exit (1);
 }
 
 
-procesaOpciones(\%opciones, \%params);
-adjustParams(\%params, \%sslopts);
-
-
-my $ua = LWP::UserAgent->new ( ssl_opts => \%sslopts );
-my $req = HTTP::Request->new ('GET' => $params{url});
-my $res = $ua->request($req);
-
-my $tree = HTML::TreeBuilder->new_from_content($res->content);
-my @ah = $tree->look_down ('_tag', 'a', 'href', qr/.+/);
-my @im = $tree->look_down ('_tag', 'img', 'src', qr/.+/);
-#for my $k (@ah) {
-#    print $k->attr('href'), "\n";
-#}
-for my $k (@im) {
-    print $k->attr('src'), "\n";
+processOptions(\%options, \%params);
+if (adjustParams(\%params, \%sslopts) == -1 ) {
+    exit (-1);
 }
+
+getConnected(\%params);
+
+push @url_to_visit, $params{url};
+
+my $url;
+while (@url_to_visit) {
+    $url = shift @url_to_visit;
+    push @visited_url, $url;
+    print "Visiting ", $url, "...\n" if ($options{debug});
+    visitUrl ($url, \%params, \%sslopts,  \%dir_found, \@url_to_visit, \@visited_url);
+#    print @url_to_visit, "\n";
+}
+my $n = keys %dir_found;
+#print "Numero de entradas: ", $n, "\n";
+for my $k (sort keys %dir_found) {
+    print $k , "\n";
+}
+#my $n = @visited_url;
+#print "Numero de entradas: ", $n, "\n";
+#for my $k (@visited_url) {
+#    print $k , "\n";
+#}
+#
+#my $n = @url_to_visit;
+#print "Numero de entradas: ", $n, "\n";
+#for (@url_to_visit) {
+#    print $_ , "\n";
+#}
+
+sub visitUrl {
+    my ($url, $p, $sslopts, $dirs, $tovisit, $visited) = @_;
+    my ($ua, $req, $res, $tree, $k, @links);
+
+    $ua = LWP::UserAgent->new ( ssl_opts => $sslopts );
+    $req = HTTP::Request->new ('GET' => $url);
+
+    # Agregamos las cabeceras necesarias a la peticion
+    for my $hn (keys %{$p->{'clheaders'}}) {
+	$req->header($hn => $p->{'clheaders'}->{$hn});
+    }
+    $res = $ua->request($req);
+
+    if ($res->code >= 200 and $res->code < 300) { # Recordar la(s) cookies enviada(s)
+	$p->{'clheaders'}->{'Cookie'} = $res->headers->{'set-cookie'} if ($res->headers->{'set-cookie'});
+    } else {
+	print 'DEBUG >> ', "El servidor respondio con el codigo ", $res->code, "\n" if ($options{debug});
+	return;
+    }
+    $tree = HTML::TreeBuilder->new_from_content($res->content);
+
+    @links = $tree->look_down ('_tag', 'a', 'href', qr/.+/);
+    push @links, $tree->look_down ('_tag', 'link', 'href', qr/.+/);
+    for $k (@links) { # Busca y revisa todas las ligas dentro del sitio
+	my $href = $k->attr('href');
+	#print $href, "\n";
+	if ($href =~ m|^$p->{url}([\w\d\._/-]+)/(\w+)\.php.*$|) {
+	    if (!$dirs->{$1}) {
+		$dirs->{$1} = 1;
+	    }
+#	    if ( (! checkURLIn ($visited, $href)) and
+#		(! checkURLIn($tovisit, $href))) {
+#		push @$tovisit, $href;
+#	    }
+	    if ( ! checkURLIn ($visited, $href)) {
+		push @$tovisit, $href;
+	    }
+	} else {
+	    print 'DEBUG >> ', $href, "\n" if ($options{debug});
+	}
+    }
+
+    $#links = -1;
+    @links = $tree->look_down ('_tag', 'img', 'src', qr/.+/);
+    push @links, $tree->look_down ('_tag', 'script', 'src', qr/.+/);
+    for $k ($tree->look_down ('_tag', 'img', 'src', qr/.+/)) { # Busca y revisa los directorios dentro del atributo 'src' de las imagenes
+	my $src = $k->attr('src');
+	if ($src =~ m|^$p->{url}([\w\d\._/-]+)/([\w\d_.-]+)\.php.*$|) {
+	    if (!$dirs->{$1}) {
+		$dirs->{$1} = 1;
+	    }
+	} else {
+	    print 'DEBUG >> ', $src, "\n" if ($options{debug});
+	}
+    }
+    for my $k ($tree->look_down ('_tag', 'form')) {
+	print 'DEBUG f>> ', $k->attr('action'), "\n" if ($options{debug});
+    }
+}
+
+#
+# Determina si la url encontrada ya se visito
+sub checkURLIn {
+    my ($aref, $entry) = @_;
+    my ($e, $mye) = ('', $entry);
+
+    for $e (@$aref) {
+	$e =~ s|^(.*)/([\w\d._-]+)\.php.*$|$1/$2.php|;
+	$mye =~ s|^(.*)/([\w\d._-]+)\.php.*$|$1/$2.php|;
+	#print '<<1>> ', $e, "\n", '<<2>> ', $mye, "\n\n";
+	return 1 if ($mye eq $e);
+    }
+    return 0;
+}
+
+
 # Hacemos peticiones con el metodo HEAD hasta determinar si en la url proporcionada
 # el servidor despacha moodle, o se redirecciona a un sitio con SSL. Tambien se detecta
 # si el certificado del servidor es emitido por alguna CA valida. Si es autofirmado o
@@ -70,7 +164,7 @@ sub adjustParams {
     # Vemos la respuesta del servidor
     if ($res->code == 200) { # Todo bien
 	$p->{loop} = 0;
-	return;
+	return 1;
     } elsif ($res->code == 301 or $res->code == 302)  { # Redirecciona a otro recurso
 	my $location = $res->headers->{'location'};
 	if ($location =~ m|^(https?)(://)([\w\d._-]+)(:\d+)?(/.*)$|) {  # Es una URL?
@@ -90,16 +184,24 @@ sub adjustParams {
 	    $p->{url} =~ s|/([^/]+)$|/$location|;
 	    $p->{uri} =~ s|/([^/]+)$|/$location|;
 	}
+	return (adjustParams($p, $ssl));     # Intentamos acceder recurso mencionado por la cabecera Location
     } elsif ($res->code == 403) {  # No tenemos acceso al recurso
 	print "No se tiene acceso a este recurso.\n";
-	exit (-1);
+	return (-1);
     } elsif ($res->code == 500) {
 	if ($res->status_line =~ /certificate verify failed/) {  # No se pudo verificar el emisor del certificado
 	    $ssl->{SSL_verify_mode} = SSL_VERIFY_NONE;
 	    $ssl->{verify_hostname} = 0;
-	    adjustParams($p, $ssl);            # Intentamos acceder al recurso sin verificar el certificado
+	    return (adjustParams($p, $ssl));            # Intentamos acceder al recurso sin verificar el certificado
 	}
     }
+}
+
+##
+## getConnected
+##
+sub getConnected {
+    my ($p) = @_;
 }
 
 ##
@@ -109,7 +211,7 @@ sub adjustParams {
 ##
 ## Regresa el hash de parametros modificados
 ##
-sub procesaOpciones {
+sub processOptions {
     my ($op, $p) = @_;
 
     if ($op->{ip} and $op->{url}) { # Solo se puede usar una, tomaremos url por default.
@@ -199,9 +301,11 @@ sub procesaOpciones {
 		exit (5);
 	    }
 	}
-    } elsif ( $op->{password} and !$op->{login} ){
-	print "Necesita un login para ese password\n";
-	exit (6);
+
+    # Entraremos a moodle como usuario guest
+    } else {
+	$p->{login} = 'guest';
+	$p->{password} = 'guest';
     }
 
     return $p;
